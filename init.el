b638acd14cd94d7114c8b4c7e6cf11a457a2474c
Matt Westerburg <matt@headerodoxy.com>
2020-06-02 22:54:27 -0500

    update

--- init.el
+++ init.el
@@ -111,6 +111,10 @@
   :ensure t
   :hook (prog-mode . rainbow-delimiters-mode))
 
+(use-package nyan-mode
+  :config
+  (nyan-mode 1))
+
 ;; (use-package vterm
 ;;   :ensure t)
 
@@ -118,13 +122,19 @@
   :ensure t
   :config
   (general-define-key
+   "M-d" 'dired
+   "M-s" 'magit-status
+   "M-C" 'uncomment-region
+   "M-c" 'comment-region
+   "M-b" 'compile
    "M-x" 'smex)
   (defconst leader-key "C-c")
   (general-create-definer leader-def :prefix leader-key)
   (leader-def
+    "g" 'goto-line
     "f" 'counsel-projectile-find-file    
     "p" 'counsel-projectile-switch-project
-    "r" 'counse≈OAl-projectile-r
+    "r" 'counsel-projectile-rg
     "a" 'counsel-projectile-ag
     "b" 'counsel-projectile-switch-to-buffer
     "n" 'neotree-toggle
@@ -160,16 +170,34 @@
 (use-package swiper
   :ensure t)
 
-(use-package evil
-  :ensure t
-  :defer t
-  :config
-  (progn
-	(setq evil-overriding-maps nil)
-	(setq evil-intercept-maps nil)
-	(setq evil-auto-indent nil)
-	(setq evil-regexp-search t)
-	(evil-mode 0)))
+;; (use-package evil
+;;   :ensure t
+;;   :defer t
+;;   :config
+;;   (progn
+;; 	(setq evil-overriding-maps nil)
+;; 	(setq evil-intercept-maps nil)
+;; 	(setq evil-auto-indent nil)
+;; 	(setq evil-regexp-search t)
+;; 	(defun toggle-evilmode ()
+;; 	  (interactive)
+;; 	  (if (bound-and-true-p evil-local-mode)
+;; 	      (progn
+;; 					; go emacs
+;; 		(evil-local-mode (or -1 1))
+;; 		(undo-tree-mode (or -1 1))
+;; 		(SET-VARIABLE 'CURSOR-TYPE 'BAR)
+;; 		)
+;; 	    (progn
+;; 					; go evil
+;; 	      (evil-local-mode (or 1 1))
+;; 	      (set-variable 'cursor-type 'box)
+;; 	      )
+;; 	    )
+;; 	  )
+ 
+;; 	(global-set-key (kbd "C-c u") 'toggle-evilmode)
+;; 	(evil-mode 0)))
 
 (use-package god-mode
   :ensure t
@@ -188,6 +216,15 @@
 (use-package dashboard
   :ensure t
   :config
+  (setq dashboard-banner-logo-title "Westy's Emacs")
+  (setq dashboard-startup-banner 'logo)
+  ;(setq dashboard-center-content t)
+  (setq dashboard-items '((recents . 5)
+			  (projects . 5)))
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-set-navigator t)
+  (setq dashboard-set-init-info t)
   (dashboard-setup-startup-hook))
 
 (use-package neotree
@@ -197,11 +234,164 @@
   (progn
     (setq projectile-switch-project-action 'neotree-projectile-action)))
 
-(use-package powerline
- :ensure t
- :init
- (powerline-center-theme))
-(add-hook 'after-init-hook 'powerline-reset)
+(use-package doom-modeline
+  :ensure t
+  :config
+  ;; How tall the mode-line should be. It's only respected in GUI.
+  ;; If the actual char height is larger, it respects the actual height.
+  (setq doom-modeline-height 25)
+
+  ;; How wide the mode-line bar should be. It's only respected in GUI.
+  (setq doom-modeline-bar-width 3)
+
+  ;; The limit of the window width.
+  ;; If `window-width' is smaller than the limit, some information won't be displayed.
+  (setq doom-modeline-window-width-limit fill-column)
+
+  ;; How to detect the project root.
+  ;; The default priority of detection is `ffip' > `projectile' > `project'.
+  ;; nil means to use `default-directory'.
+  ;; The project management packages have some issues on detecting project root.
+  ;; e.g. `projectile' doesn't handle symlink folders well, while `project' is unable
+  ;; to hanle sub-projects.
+  ;; You can specify one if you encounter the issue.
+  (setq doom-modeline-project-detection 'project)
+
+  ;; Determines the style used by `doom-modeline-buffer-file-name'.
+  ;;
+  ;; Given ~/Projects/FOSS/emacs/lisp/comint.el
+  ;;   auto => emacs/lisp/comint.el (in a project) or comint.el
+  ;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
+  ;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
+  ;;   truncate-with-project => emacs/l/comint.el
+  ;;   truncate-except-project => ~/P/F/emacs/l/comint.el
+  ;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
+  ;;   truncate-all => ~/P/F/e/l/comint.el
+  ;;   truncate-nil => ~/Projects/FOSS/emacs/lisp/comint.el
+  ;;   relative-from-project => emacs/lisp/comint.el
+  ;;   relative-to-project => lisp/comint.el
+  ;;   file-name => comint.el
+  ;;   buffer-name => comint.el<2> (uniquify buffer name)
+  ;;
+  ;; If you are experiencing the laggy issue, especially while editing remote files
+  ;; with tramp, please try `file-name' style.
+  ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
+  (setq doom-modeline-buffer-file-name-style 'auto)
+
+  ;; Whether display icons in the mode-line. Respects `all-the-icons-color-icons'.
+  ;; While using the server mode in GUI, should set the value explicitly.
+  (setq doom-modeline-icon (display-graphic-p))
+
+  ;; Whether display the icon for `major-mode'. Respects `doom-modeline-icon'.
+  (setq doom-modeline-major-mode-icon t)
+
+  ;; Whether display the colorful icon for `major-mode'.
+  ;; Respects `doom-modeline-major-mode-icon'.
+  (setq doom-modeline-major-mode-color-icon t)
+
+  ;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
+  (setq doom-modeline-buffer-state-icon t)
+
+  ;; Whether display the modification icon for the buffer.
+  ;; Respects `doom-modeline-icon' and `doom-modeline-buffer-state-icon'.
+  (setq doom-modeline-buffer-modification-icon t)
+
+  ;; Whether to use unicode as a fallback (instead of ASCII) when not using icons.
+  (setq doom-modeline-unicode-fallback nil)
+
+  ;; Whether display the minor modes in the mode-line.
+  (setq doom-modeline-minor-modes nil)
+
+  ;; If non-nil, a word count will be added to the selection-info modeline segment.
+  (setq doom-modeline-enable-word-count nil)
+
+  ;; Major modes in which to display word count continuously.
+  ;; Also applies to any derived modes. Respects `doom-modeline-enable-word-count'.
+  ;; If it brings the sluggish issue, disable `doom-modeline-enable-word-count' or
+  ;; remove the modes from `doom-modeline-continuous-word-count-modes'.
+  (setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
+
+  ;; Whether display the buffer encoding.
+  (setq doom-modeline-buffer-encoding t)
+
+  ;; Whether display the indentation information.
+  (setq doom-modeline-indent-info nil)
+
+  ;; If non-nil, only display one number for checker information if applicable.
+  (setq doom-modeline-checker-simple-format t)
+
+  ;; The maximum number displayed for notifications.
+  (setq doom-modeline-number-limit 99)
+
+  ;; The maximum displayed length of the branch name of version control.
+  (setq doom-modeline-vcs-max-length 12)
+
+  ;; Whether display the perspective name. Non-nil to display in the mode-line.
+  (setq doom-modeline-persp-name t)
+
+  ;; If non nil the default perspective name is displayed in the mode-line.
+  (setq doom-modeline-display-default-persp-name nil)
+
+  ;; If non nil the perspective name is displayed alongside a folder icon.
+  (setq doom-modeline-persp-icon t)
+
+  ;; Whether display the `lsp' state. Non-nil to display in the mode-line.
+  (setq doom-modeline-lsp t)
+
+  ;; Whether display the GitHub notifications. It requires `ghub' package.
+  (setq doom-modeline-github nil)
+
+  ;; The interval of checking GitHub.
+  (setq doom-modeline-github-interval (* 30 60))
+
+  ;; Whether display the modal state icon.
+  ;; Including `evil', `overwrite', `god', `ryo' and `xah-fly-keys', etc.
+  (setq doom-modeline-modal-icon t)
+
+  ;; Whether display the mu4e notifications. It requires `mu4e-alert' package.
+  (setq doom-modeline-mu4e nil)
+
+  ;; Whether display the gnus notifications.
+  (setq doom-modeline-gnus t)
+
+  ;; Wheter gnus should automatically be updated and how often (set to 0 or smaller than 0 to disable)
+  (setq doom-modeline-gnus-timer 2)
+
+  ;; Wheter groups should be excludede when gnus automatically being updated.
+  (setq doom-modeline-gnus-excluded-groups '("dummy.group"))
+
+  ;; Whether display the IRC notifications. It requires `circe' or `erc' package.
+  (setq doom-modeline-irc t)
+
+  ;; Function to stylize the irc buffer names.
+  (setq doom-modeline-irc-stylize 'identity)
+
+  ;; Whether display the environment version.
+  (setq doom-modeline-env-version t)
+  ;; Or for individual languages
+  (setq doom-modeline-env-enable-python t)
+  (setq doom-modeline-env-enable-ruby t)
+  (setq doom-modeline-env-enable-perl t)
+  (setq doom-modeline-env-enable-go t)
+  (setq doom-modeline-env-enable-elixir t)
+  (setq doom-modeline-env-enable-rust t)
+
+  ;; Change the executables to use for the language version string
+  (setq doom-modeline-env-python-executable "python") ; or `python-shell-interpreter'
+  (setq doom-modeline-env-ruby-executable "ruby")
+  (setq doom-modeline-env-perl-executable "perl")
+  (setq doom-modeline-env-go-executable "go")
+  (setq doom-modeline-env-elixir-executable "iex")
+  (setq doom-modeline-env-rust-executable "rustc")
+
+  ;; What to dispaly as the version while a new one is being loaded
+  (setq doom-modeline-env-load-string "...")
+
+  ;; Hooks that run before/after the modeline version string is updated
+  (setq doom-modeline-before-update-env-hook nil)
+  (setq doom-modeline-after-update-env-hook nil)
+  :init
+  (doom-modeline-mode 1))
 
 (use-package expand-region
    :ensure t
@@ -253,13 +443,13 @@
   (smex-initialize)
   (global-set-key (kbd "M-x") 'smex))
 
-(use-package flycheck-rust
-  :ensure t)
-
 (use-package racer
   :ensure t)
 
 (use-package cargo
+  :ensure t)
+
+(use-package rustic
   :ensure t)
 
 (use-package rust-mode
@@ -284,7 +474,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rjsx-mode tide company-lsp lsp-ui lsp-mode company rust-mode use-package))))
+    (lsp-mode rustic which-key use-package tide smex rjsx-mode rainbow-delimiters racer powerline neotree magit lsp-ui general flycheck-rust expand-region exec-path-from-shell evil editorconfig dashboard counsel-projectile company-lsp cargo avy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -326,4 +516,6 @@
 ;;
 ;; KEYMAP CONFIGURATION
 ;;
+
+
 
